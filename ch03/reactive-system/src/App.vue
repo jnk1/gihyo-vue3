5a14b59deb73aaf407d3ce05617a87d0d4e01f37
Junki Hori <junki326+github@gmail.com>
2022-12-12 18:46:40 +0900

    3.2 リアクティブシステム

---
+++ ch03/reactive-system/src/App.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ref } from "vue";
+
+const now = new Date(); // 現在日時
+const nowStr = now.toLocaleTimeString();
+let timeStr = nowStr; // 現在時刻文字列をテンプレート変数として用意
+const timeStrRef = ref(nowStr);
+
+function changeTime(): void {
+  // 新しい時刻に変更する関数
+  const newTime = new Date();
+  const newTimeStr = newTime.toLocaleTimeString();
+  timeStr = newTimeStr;
+  timeStrRef.value = newTimeStr;
+}
+
+setInterval(changeTime, 1000);
+</script>
+
+<template>
+  <p>現在時刻: {{ timeStr }}</p>
+  <p>現在時刻(ref): {{ timeStrRef }}</p>
+</template>
